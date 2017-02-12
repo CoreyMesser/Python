c0e363767d45fde7fac46bca0a33888d134f212a
Corey Messer <tiredorangecat@hotmail.com>
2017-02-11 21:44:51 -0600

    vowel_remover
    remove all vowels from a word

---
+++ vowel_remove.py
@@ -0,0 +1,21 @@
+#word = 'pibble wIbble 3 fIbblEtrIbble'
+
+def remove_vowel():
+    word = input("Enter a word: ")
+    vowel_l = ['i', 'e', 'a', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    w_list = list(word)
+    vowel_sp = []
+    f_word = []
+        
+    for vowel in vowel_l:
+        while vowel in w_list:
+            vowel_sp = w_list.remove(vowel)
+    #print(w_list)
+    
+    f_word = ''.join(w_list)
+    print(f_word)
+
+while True:
+    remove_vowel()
+    if remove_vowel == "DONE":
+        break 
